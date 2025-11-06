233f450017e78ce666d1f6f4f7c161f70909eab4
DhanrajKamble5205 <dhanrajkamble5205@gmail.com>
2025-11-06 12:49:23 +0530

    login.ts- Added logout feature

--- pages/login.ts
+++ pages/login.ts
@@ -5,13 +5,15 @@ export class LoginPage {
     readonly page:Page
     log_username: any // this is a type of value, now it can access any type of value
     log_password: any
-    log_button: any
+    login_button: any
+    logout_button: any
 
     constructor(page:Page) { // declare the constructor to access class properties.
         this.page = page
         this.log_username = page.getByRole('textbox', { name: 'Username' }) // locator element
         this.log_password = page.getByRole('textbox', { name: 'Password' })
-        this.log_button = page.getByRole('button', { name: ' Login' })
+        this.login_button = page.getByRole('button', { name: ' Login' })
+        this.logout_button = page.getByRole('link', { name: 'Logout' })
     }
 
     async login_url(){
@@ -20,6 +22,7 @@ export class LoginPage {
     async login(username: String, password: any){ // create instance of the object and passing the value
         await this.log_username.fill(username)
         await this.log_password.fill(password)
-        await this.log_button.click()
+        await this.login_button.click()
+        await this.logout_button.click()
     }
 }
